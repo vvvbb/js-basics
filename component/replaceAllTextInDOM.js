f2cc9db4cfe755745274eeb8d8f99b144be22545
vincent <vincent.bartissol@colorz.fr>
2022-05-08 17:19:25 +0200

    export default module

--- component/replaceAllTextInDOM.js
+++ component/replaceAllTextInDOM.js
@@ -1,6 +1,10 @@
-let docAll = document.querySelectorAll("p, h1, h2, h3, h4 ,h5, span");
-console.log("docAll  :", docAll);
+export default () => {
 
-[...docAll].map(txt => txt.innerText = txt.innerText.replace(/[eE]/g,"😛"));
-// [...docAll].map(txt => txt.innerText = txt.innerText.replace(/[aA]/g,"👍"));
-[...docAll].map(txt => txt.innerText = txt.innerText.replace(/[oO]/g,"😗"));
+    let docAll = document.querySelectorAll("p, h1, h2, h3, h4 ,h5, span");
+    console.log("docAll  :", docAll);
+
+    [...docAll].map(txt => txt.innerText = txt.innerText.replace(/[eE]/g, "😛"));
+    // [...docAll].map(txt => txt.innerText = txt.innerText.replace(/[aA]/g,"👍"));
+    [...docAll].map(txt => txt.innerText = txt.innerText.replace(/[oO]/g, "😗"));
+
+}
--- script.js
+++ script.js
@@ -5,10 +5,13 @@ import tutomodernJS from "./tuto-modernJS.js";
 
 
 import randomuser from "./component/randomuser/randomuser.js";
+// import replaceAllTextInDOM from "./component/randomuser/randomuser.js";
+import replaceAllTextInDOM from './component/replaceAllTextInDOM.js'
 
 
 // jsquiz()
 // firstscript()
 // tutomodernJS()
+replaceAllTextInDOM();
 
 randomuser()
